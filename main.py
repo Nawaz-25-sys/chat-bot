4ecfc378ad7ab7bf7166759a963fc632251df51c
Nawaz-25-sys <smohamadnawaz@gmail.com>
2026-02-05 15:18:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request , render_template
+from google import genai
+
+client = genai.Client(api_key="AIzaSyCm5nbNuL18WhXaMsKXI3zF6pY-qUfAQwY")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    return client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=request.json["msg"],
+    ).text
+app.run(port=5000)
